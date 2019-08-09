1541dffeecfda60e9d307c860a65d05a9d1c5081
Nicolas Meinen <nicolas.meinen@wirelesscar.com>
2019-08-09 16:57:45 +0200

    add some category stuff playing on haskell

---
+++ haskell/tst.hs
@@ -0,0 +1,37 @@
+-- useless helpers
+catenate snd fst = fst ++ snd
+catenateYou      = catenate ", you"
+catenateYouOpt   = Just . catenateYou
+
+-- same as fmap but infix
+-- remember: Functor are simply mappers from one cat to another
+-- in hask case, they are the same, so we have an Endofunctor
+-- it maps object: va E Hask \exists Fa E Hask
+-- and arrows. arrows are from fmap
+functor   :: Maybe String -> Maybe String
+functor a = catenateYou <$> a
+
+-- calling the applicative star
+-- remember: an Applicative is just a lax monoidal functor
+-- a lax monoidal functor has two isomorphisms:
+-- a :: 1h -> F 1h
+-- e :: F([X,Y]) -> ([FX, FY])
+-- in this case, the star is doing
+-- map :: f (a -> b) -> Fa -> Fb
+-- so, a functor holding a function and applying it to the functor outside
+applicative   :: Maybe String -> Maybe String
+applicative a = Just catenateYou <*> a
+
+-- calling the kleisli arrow
+-- remember: the monad is the easiest
+-- it is just a Kleisli triple (F, n, e) where n, e are natural transformations and 
+-- where n :: T2 -> T (join)
+-- where e :: 1 -> T  (unit)
+-- in this case, the kleisli arrow is just acting as n; 
+-- the unit would be the constructor of the datatype Just
+monad   :: Maybe String -> Maybe String
+monad a = a >>= catenateYouOpt
+
+-- just to show some point free notation
+do_all :: Maybe String -> Maybe String
+do_all = applicative . functor . monad
