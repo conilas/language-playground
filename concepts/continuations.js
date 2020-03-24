fd7960332e6a71bd6a35c83e3c33b616060a3425
Nicolas Meinen <meinen.nicolas@gmail.com>
2020-03-24 18:26:45 +0100

    Update continuations.js

--- concepts/continuations.js
+++ concepts/continuations.js
@@ -1,9 +1,12 @@
 //from https://pdfs.semanticscholar.org/f2b2/8191102f553fa4095fb4b8a07d8eed4bd259.pdf
+const head = (values) => values[0]
+const tail = (values) => values.slice(1)
+
 const rememberuptolast_ret = (comp, list) => {
   const fn = (values) => (kont) => {
     if (values.length == 0) return kont()
     
-    return fn(values.slice(1))(() => comp == values[0] ? values.slice(1) : kont())
+    return fn(tail(values))(() => comp == head(values) ? tail(values) : kont())
   }
   
   return fn(list)(() => list)
